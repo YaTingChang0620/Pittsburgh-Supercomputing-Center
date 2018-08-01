2c2117617b136202ed6b015642d66a23e5e89193
YA-TING CHANG <yating@br005.pvt.bridges.psc.edu>
2018-08-01 14:33:13 -0400

    similarity_gmm

--- fingerprint_gen.R
+++ fingerprint_gen.R
@@ -2,6 +2,8 @@ library(rJava)
 library(rcdklibs)
 library(rcdk)
 library(iterators)
+library(fingerprint)
+library(tidyverse)
 
 
 # read sdf file and compute the fingerprint (ECFP)
@@ -14,7 +16,7 @@ fingerprint_gen <- function(sdffile){
     i=i+1
   }
   molecule_fing <- lapply(molecule,get.fingerprint,type='circular')
-  return(molecule_fing)
+  return(as_data_frame(fp.factor.matrix(molecule_fing)))
 }
 
 # five proteint target ID(from ChEMBL): 100414,11910,11359,11267,193
@@ -24,3 +26,10 @@ p11359 <- fingerprint_gen('11359.sdf')
 p11267 <- fingerprint_gen('11267.sdf')
 p193 <- fingerprint_gen('193.sdf')
 
+# write multiple dataframes to csv
+myList <- list(p100414 = p100414, p11910 = p11910, 
+               p11359 = p11359, p11267 = p11267, p193 = p193)
+for(i in names(myList)){
+  write.csv(myList[[i]],paste0(i,".csv"))
+}
+  
