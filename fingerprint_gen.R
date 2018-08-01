e2efe5e8936b064a17f9314233b6d25828562ead
YA-TING CHANG <yating@br006.pvt.bridges.psc.edu>
2018-07-31 22:28:58 -0400

    generate fingerprint using R

--- fingerprint_gen.R
+++ fingerprint_gen.R
@@ -2,18 +2,25 @@ library(rJava)
 library(rcdklibs)
 library(rcdk)
 library(iterators)
-# data("bpdata")
-# target.mols <- parse.smiles(bpdata[,1])
-# target.fing <- lapply(target.mols, get.fingerprint, type='circular')
-# bpdata[,1]
 
-# mol_100414 <- iload.molecules('100414.smiles',type='smi')
-# while(hasNext(mol_100414)) {
-#   mol100414 <- nextElem(mol_100414)
-# }
 
-mol_100414 <- iload.molecules('100414.sdf',type='sdf')
-while(hasNext(mol_100414)) {
-  mol100414 <- nextElem(mol_100414)
+# read sdf file and compute the fingerprint (ECFP)
+fingerprint_gen <- function(sdffile){
+  molecule = list()
+  i=1
+  mol_i = iload.molecules(sdffile,type='sdf')
+  while(hasNext(mol_i)){
+    molecule[i] <- nextElem(mol_i)
+    i=i+1
+  }
+  molecule_fing <- lapply(molecule,get.fingerprint,type='circular')
+  return(molecule_fing)
 }
+
+# five proteint target ID(from ChEMBL): 100414,11910,11359,11267,193
+p100414 <- fingerprint_gen('100414.sdf')
+p11910 <- fingerprint_gen('11910.sdf')
+p11359 <- fingerprint_gen('11359.sdf')
+p11267 <- fingerprint_gen('11267.sdf')
+p193 <- fingerprint_gen('193.sdf')
 
